95a75cd44c306305a469f38b3e3c82c0e63a1787
Griffone <thegriffones@gmail.com>
2021-08-28 16:11:24 +0200

    Remove slash commands

--- src/bot.py
+++ src/bot.py
@@ -7,21 +7,21 @@ from argparse import ArgumentParser
 from os import error
 
 import discord
+from discord.ext import commands
 import toml
 from discord.ext.commands import Bot as DiscordBot
 from discord.mentions import AllowedMentions
-from discord_slash import SlashCommand, SlashContext
 
 from music import MusicCog
+import response
 
 BOT_VERSION = '0.1.2'
 
 bot = DiscordBot(
-	'.',
+	'b.',
 	intents=discord.Intents.default(),
 	allowed_mentions=AllowedMentions(users=True),
 )
-slash = SlashCommand(bot)
 logger = logging.getLogger(__name__)
 
 
@@ -34,21 +34,35 @@ async def on_ready():
 	logger.info('%s is ready', bot.user.name)
 
 
-@slash.slash()
-async def version(ctx: SlashContext):
-	'''
-	Print the current bot version.
-	'''
-	logger.info('displaying bot version')
-	await ctx.send(f"My version is `{BOT_VERSION}`.")
+@bot.before_invoke
+async def log_command(ctx: commands.Context):
+	logger.info('invoking "%s"', ctx.message)
 
 
-@slash.slash()
-async def rate(ctx: SlashContext, user: discord.Member):
+async def handle_command_error(ctx: commands.Context, err: Exception):
+	if isinstance(err, commands.CommandError):
+		if isinstance(err, commands.errors.MissingRequiredArgument):
+			await ctx.reply(f'Parameter <{err.param}> required.')
+		else:
+			await ctx.reply(str(err))
+	else:
+		await ctx.reply(':poop: ERROR :poop:')
+		raise err
+
+
+@bot.command()
+async def status(ctx: commands.Context):
+	'''
+	Print the bot status.
+	'''
+	await ctx.send(f"Running version `{BOT_VERSION}`.")
+
+
+@bot.command()
+async def rate(ctx: commands.Context, user: discord.Member):
 	'''
 	Rate the provided user out of 10.
 	'''
-	logger.info('rating %s (id: %s)', user.display_name, user.id)
 	if user.id == 305440304528359424 or user == bot.user:
 		rating = 10
 	else:
@@ -56,11 +70,10 @@ async def rate(ctx: SlashContext, user: discord.Member):
 	await ctx.send(f'{user.mention} is {rating}/10.')
 
 
+
 def run_bot():
 	# set up arguments
 	parser = ArgumentParser(prog='bottica', description='Run a discord bot named "Bottica".')
-	parser.add_argument('--sync', action='store_true', help='Synchronize bot commands with discord.')
-	parser.add_argument('--debug-guild', type=int, help='Debug Discord Guild id to use, will override one provided in config.')
 	parser.add_argument('--token', type=str, help='Discord API token to use, will override one provided in config.')
 	parser.add_argument('--log', choices=('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL',))
 
@@ -78,22 +91,17 @@ def run_bot():
 		print('Add it to "config.toml" or provide with --token.')
 
 	# set up logging
-	log_level = args.log or config.get('log') or 0
+	log_level = args.log or config.get('log') or logging.NOTSET
 	logging.basicConfig(
-		format='%(asctime)s %(levelname)s %(name)s: %(message)s',
+		format='%(asctime)s:%(levelname)s:%(name)s: %(message)s',
 		level=log_level,
 	)
 	logger.debug('set logging level to %s', log_level)
 	logging.getLogger('discord').setLevel('WARNING')
-
-	slash.debug_guild = args.debug_guild or config.get('debug_guild')
-	logger.debug('debug_guild=%s', slash.debug_guild)
-
-	if args.sync:
-		logger.info("synchronizing commands")
-		bot.loop.create_task(slash.sync_all_commands())
 	
 	bot.add_cog(MusicCog(bot))
+
+	bot.on_command_error = handle_command_error
 	bot.run(config['token'])
 
 
--- src/music.py
+++ src/music.py
@@ -9,7 +9,6 @@ from typing import Sequence
 
 import discord
 from discord.ext import commands
-from discord_slash import SlashContext, cog_ext
 from youtube_dl import YoutubeDL
 
 import response
@@ -20,21 +19,22 @@ CACHE_FOLDER = DATA_FOLDER + 'cache/'
 logger = logging.getLogger(__name__)
 
 
-async def _validate_context(ctx: SlashContext) -> bool:
+async def check_author_is_voice_connected(ctx: commands.Context) -> bool:
 	if ctx.author.voice is None:
-		embed = discord.Embed('You need to be in a voice channel.')
-		await ctx.send(embed=embed)
 		return False
 	
 	if ctx.voice_client is None:
 		await ctx.author.voice.channel.connect()
 	else:
 		await ctx.voice_client.move_to(ctx.author.voice.channel)
-	
 	return True
 
 
-class MusicCog(commands.Cog):
+def check_author_is_dj(ctx: commands.Context) -> bool:
+	return '@dj' in ctx.author.roles
+
+
+class MusicCog(commands.Cog, name='Music'):
 	def __init__(self, bot: commands.Bot) -> None:
 		self.bot = bot
 		ytdl_options = {
@@ -81,61 +81,53 @@ class MusicCog(commands.Cog):
 		return self.voice_client is not None and self.voice_client.is_playing()
 
 
-	@cog_ext.cog_slash()
-	async def play(self, ctx: SlashContext, query: str):
+	@commands.command(aliases=('p',))
+	@commands.check(check_author_is_voice_connected)
+	async def play(self, ctx: commands.Context, query: str):
 		'''
 		Play provided input.
 		'''
-		logger.info('playing %s', query)
-		try:
-			if not await _validate_context(ctx):
-				return
-			self.voice_client = ctx.voice_client
-			# download should be run asynchronously as to avoid blocking the bot
-			info = await self.bot.loop.run_in_executor(
-				None,
-				lambda: self.ytdl.extract_info(query, download=False),
-			)
-			if info:
-				await ctx.send(random.choice(response.SUCCESSES))
-			else:
-				return await ctx.send(random.choice(response.FAILS))
+		self.voice_client = ctx.voice_client
+		# download should be run asynchronously as to avoid blocking the bot
+		info = await self.bot.loop.run_in_executor(
+			None,
+			lambda: self.ytdl.extract_info(query, download=False),
+		)
+		if info:
+			await ctx.reply(random.choice(response.SUCCESSES))
+		else:
+			return await ctx.reply(random.choice(response.FAILS))
 
-			if 'entries' in info:
-				await self._queue_audio([entry for entry in info['entries']])
-			else:
-				await self._queue_audio((info,))
-
-			# actual playing will happen once audio is available
-		except Exception as e:
-			traceback.print_exception(type(e), e, e.__traceback__)
+		if 'entries' in info:
+			await self._queue_audio([entry for entry in info['entries']])
+		else:
+			await self._queue_audio((info,))
+		# actual playing will happen once audio is available
 	
 
-	@cog_ext.cog_slash()
-	async def shuffle(self, ctx: SlashContext):
+	@commands.command()
+	async def shuffle(self, ctx: commands.context):
 		'''
 		Toggle shuffling of the queued playlist.
 		'''
-		logger.info('toggling shuffle')
 		self.is_shuffling = not self.is_shuffling
 		if self.is_shuffling:
 			resp = 'Shuffling queued songs.'
 		else:
 			resp = 'Playing queued songs in order.'
-		await ctx.send(resp)
+		await ctx.reply(resp)
 	
 
-	@cog_ext.cog_slash()
-	async def next(self, ctx: SlashContext):
+	@commands.command()
+	async def next(self, ctx: commands.Context):
 		'''
 		Skip the current song.
 		'''
-		logger.info('skipping song')
 		if not self.is_playing():
-			return await ctx.send("I'm not playing anything." + random.choice(response.FAILS))
+			return await ctx.reply("I'm not playing anything." + random.choice(response.FAILS))
 		
 		self.play_next(pause=True)
-		await ctx.send(random.choice(response.SUCCESSES))
+		await ctx.reply(random.choice(response.SUCCESSES))
 	
 
 	def play_next(self, pause=False):
